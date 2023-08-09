c53b0ceac931521d045f8dcb03b56eefb3d0be52
hndeau <henry.nadeau@unh.edu>
2023-08-09 12:58:57 -0400

    working version

--- driver.py
+++ driver.py
@@ -6,21 +6,21 @@ import random
 import sys
 import threading
 import time
-
 import aiohttp
 import keyboard
+
 from screeninfo import get_monitors
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from concurrent.futures import ThreadPoolExecutor
 
 from utility_helpers import load_from_json, save_to_json
 
 # --------- CONFIG AND INITIALIZATION -----------
 
-# Load the config file
 with open("config.json", "r") as file:
     config = json.load(file)
 
@@ -31,21 +31,14 @@ if not API_KEY:
     logging.error("API key not provided in config.")
     sys.exit(1)
 
-# Constants from Config
 BASE_URL = config['BASE_URL']
 CHANNEL_ID = config['CHANNEL_ID']
 BROWSER_WINDOW_SIZE = tuple(config['BROWSER_WINDOW_SIZE'])
 
-# Setup Logging
 logger = logging.getLogger(__name__)
 logging_level = logging.DEBUG if DEBUG_MODE else logging.WARNING
 logging.basicConfig(level=logging_level)
 
-# Global Variables
-monitors = []
-random.seed(time.time())
-threads = []
-STOP_THREADS = False
 VIDEO_CACHE = load_from_json() or {}
 TOP_N = config.get('TOP_N', 100)  # default to 100 if not specified in config
 
@@ -57,27 +50,24 @@ class Monitor:
         self.current_url = url
 
     def is_playing_video(self):
-        if STOP_THREADS:
-            return False
         return self._video_has_ended()
 
     def play_video(self, url):
-        if STOP_THREADS:
-            return
-        self.browser.get(url)
-        self.current_url = url
-        player = WebDriverWait(self.browser, 15).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "video.video-stream"))
-        )
-        player.send_keys('F')
-        player.send_keys(Keys.SPACE)
+        try:
+            self.browser.get(url)
+            self.current_url = url
+            player = WebDriverWait(self.browser, 15).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "video.video-stream"))
+            )
+            player.send_keys('F')
+            player.send_keys(Keys.SPACE)
+        except Exception as e:
+            logger.error(f"Error playing video: {e}")
 
     def close(self):
         self.browser.quit()
 
     def is_page_loaded(self):
-        if STOP_THREADS:
-            return False
         """Check if the page is completely loaded."""
         try:
             return self.browser.execute_script("return document.readyState") == "complete"
@@ -86,10 +76,7 @@ class Monitor:
             return False
 
     def _video_has_ended(self):
-        if STOP_THREADS:
-            return False
         try:
-            # Wait for the element to be present
             WebDriverWait(self.browser, 15).until(
                 EC.presence_of_element_located((By.ID, "movie_player"))
             )
@@ -103,10 +90,74 @@ class Monitor:
             return False
 
 
-def cleanup_browsers():
-    """Close all browser windows."""
-    for monitor in monitors:
-        monitor.close()
+class MonitorManager:
+    def __init__(self):
+        self.monitors = []
+        self.STOP_THREADS = False
+
+    def is_stopped(self):
+        return self.STOP_THREADS
+
+    def stop(self):
+        self.STOP_THREADS = True
+        # Pause for a brief moment to allow any ongoing operations to complete
+        time.sleep(2)
+        self.cleanup_browsers()
+
+    def cleanup_browsers(self):
+        """Close all browser windows."""
+        for monitor in self.monitors:
+            monitor.close()
+
+    async def fetch_all_videos(self):
+        return await asyncio.gather(fetch_live_videos(), fetch_top_100_videos())
+
+    async def monitor_video_statuses(self, monitor):
+        while not self.is_stopped():
+            live_videos, top_videos = await self.fetch_all_videos()
+            if monitor.is_page_loaded() and not monitor.is_playing_video():
+                for live_video in live_videos:
+                    if live_video not in [m.current_url for m in self.monitors]:
+                        monitor.play_video(live_video)
+                        break
+                else:
+                    selected_video = random.choice(top_videos)
+                    while selected_video in [m.current_url for m in self.monitors]:
+                        selected_video = random.choice(top_videos)
+                    monitor.play_video(selected_video)
+            await asyncio.sleep(5)
+
+    def init_browser_and_return_monitor(self, monitor):
+        """This method initializes the browser and returns the monitor instance."""
+        browser = webdriver.Firefox()
+        screen_width, screen_height = monitor.width, monitor.height
+        window_width, window_height = BROWSER_WINDOW_SIZE
+        position_x = (screen_width - window_width) // 2 + monitor.x
+        position_y = (screen_height - window_height) // 2 + monitor.y
+
+        browser.set_window_size(*BROWSER_WINDOW_SIZE)
+        browser.set_window_position(position_x, position_y)
+
+        monitor_instance = Monitor(browser)
+        self.monitors.append(monitor_instance)
+        return monitor_instance
+
+    async def monitor_all_screens(self):
+        detected_monitors = get_monitors()
+
+        # Use ThreadPoolExecutor to initialize browsers in parallel
+        with ThreadPoolExecutor(max_workers=len(detected_monitors)) as executor:
+            initialized_monitors = list(executor.map(self.init_browser_and_return_monitor, detected_monitors))
+
+        # Once all browsers are initialized, start the asynchronous monitoring
+        await self.monitor_video_statuses_on_all_monitors(initialized_monitors)
+
+    async def monitor_video_statuses_on_all_monitors(self, initialized_monitors):
+        await asyncio.gather(*[self.monitor_video_statuses(monitor) for monitor in initialized_monitors])
+
+    def key_listener(self):
+        keyboard.wait('esc')
+        self.stop()
 
 
 # -------- YOUTUBE API HANDLING -----------
@@ -159,88 +210,16 @@ async def fetch_top_100_videos():
 
 
 # ---------- MAIN EXECUTION FUNCTIONS ------------
-async def monitor_video_statuses(monitor):
-    while True:
-        if STOP_THREADS:
-            return
-        live_videos = await fetch_live_videos()
-        top_videos = await fetch_top_100_videos()
-        if monitor.is_page_loaded() and not monitor.is_playing_video():
-            for live_video in live_videos:
-                if live_video not in [m.current_url for m in monitors]:
-                    monitor.play_video(live_video)
-                    break
-            else:
-                selected_video = random.choice(top_videos)
-                while selected_video in [m.current_url for m in monitors]:
-                    selected_video = random.choice(top_videos)
-                monitor.play_video(selected_video)
-        await asyncio.sleep(5)
-
-
-def key_listener():
-    """Listens for a key press to stop all threads."""
-    global STOP_THREADS
-    keyboard.wait('esc')
-    logger.debug('Escape key pressed.')  # waits until the 'esc' key is pressed
-    STOP_THREADS = True
-    logger.debug('STOP_THREADS set to True.')
-    cleanup_browsers()
-    loop.stop()
-
-
-def handle_monitor(monitor):
-    """Function to handle each monitor in its own thread."""
-    global STOP_THREADS
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    browser = webdriver.Firefox()
-
-    # Get the screen resolution for the current monitor
-    screen_width, screen_height = monitor.width, monitor.height
-    window_width, window_height = BROWSER_WINDOW_SIZE
-
-    # Calculate top-left position to center the window on the screen
-    position_x = (screen_width - window_width) // 2 + monitor.x
-    position_y = (screen_height - window_height) // 2 + monitor.y
-
-    browser.set_window_size(*BROWSER_WINDOW_SIZE)
-    browser.set_window_position(position_x, position_y)
-
-    monitor_instance = Monitor(browser)
-    monitors.append(monitor_instance)
-
-    while not STOP_THREADS:
-        logger.debug('handle_monitor loop running.')
-        loop.run_until_complete(monitor_video_statuses(monitor_instance))
-    loop.close()
-
-
-def open_browsers_for_monitors():
-    global threads
-    detected_monitors = get_monitors()
-    logger.debug(f"Detected screens: {[monitor for monitor in detected_monitors]}")
-
-    for monitor in detected_monitors:
-        thread = threading.Thread(target=handle_monitor, args=(monitor,))
-        thread.start()
-        threads.append(thread)
-
-
-async def main_execution():
-    open_browsers_for_monitors()
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    manager = MonitorManager()
+
+    # Start the key listener thread first
+    key_thread = threading.Thread(target=manager.key_listener)
+    key_thread.start()
+
     try:
-        loop.run_until_complete(main_execution())
+        asyncio.run(manager.monitor_all_screens())
     finally:
-        # Start the key listener thread
-        key_thread = threading.Thread(target=key_listener)
-        key_thread.start()
-        # Ensure all threads are joined and closed properly
-        for thread in threads:
-            thread.join()
         key_thread.join()
