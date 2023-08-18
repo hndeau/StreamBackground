be66301d610ca985a5c499e963cd699ffba47aa5
hndeau <henry.nadeau@unh.edu>
2023-08-18 13:27:11 -0400

    more logging

--- browser_handler.py
+++ browser_handler.py
@@ -12,20 +12,6 @@ from screeninfo import get_monitors
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-import asyncio
-import threading
-
-import keyboard
-import time
-import queue
-import json
-import logging
-import sys
-import aiohttp
-from screeninfo import get_monitors
-from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -36,24 +22,9 @@ from concurrent.futures import ThreadPoolExecutor
 from yt_scrape import monitor_youtube_streams
 from utility_helpers import load_from_json
 from cacher import CacheManager
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.ui import WebDriverWait
-from concurrent.futures import ThreadPoolExecutor
-
-from yt_scrape import monitor_youtube_streams
-from utility_helpers import load_from_json
-from cacher import CacheManager
 
 
 # -------- LOGGING -----------
-def load_config():
-    with open("config.json", "r") as file:
-        c = json.load(file)
-
-    debug_mode = "--debug" in sys.argv or "-d" in sys.argv
-    return c, debug_mode
-
-
 def setup_logging():
     debug_general = 25
     debug_selenium = 15
@@ -77,11 +48,19 @@ def setup_logging():
     return logging.getLogger(__name__)
 
 
-if sys.platform == 'win32':
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+def load_config():
+    with open("config.json", "r") as file:
+        c = json.load(file)
+
+    debug_mode = "--debug" in sys.argv or "-d" in sys.argv
+    return c, debug_mode
+
 
 config, DEBUG_MODE = load_config()
 logger = setup_logging()
+
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 API_KEY = config['API_KEY']
 if not API_KEY:
@@ -99,50 +78,8 @@ POPULAR_VIDEOS_LIMIT = config.get('POPULAR_VIDEOS_LIMIT', 100)
 
 # -------- MONITOR AND BROWSER HANDLING -----------
 
-
-class Monitor:
-    def __init__(self, browser, url=None):
-        self.browser = browser
-        self.current_url = url
-
-    def play_video(self, url):
-        if self.current_url == url:  # Prevent replaying the same video
-            return
-        try:
-            logger.debug_general(f'Video Started: {url}')
-            self.browser.get(url)
-            self.current_url = url
-            player = WebDriverWait(self.browser, 4).until(  # can probably be lower
-                ec.element_to_be_clickable((By.CSS_SELECTOR, "video.video-stream"))
-            )
-            player.send_keys('F', Keys.SPACE)
-        except Exception as e:
-            logger.error(f"Error playing video: {e}")
-
-    def close(self):
-        self.browser.quit()
-
-    def is_page_loaded(self):
-        try:
-            return self.browser.execute_script("return document.readyState") == "complete"
-        except Exception as e:
-            logger.error(f"Error checking page load status: {e}")
-            return False
-
-    def is_playing_video(self):
-        try:
-            # Wait for the video player element to be present on the page
-            WebDriverWait(self.browser, 4).until(  # can probably be lower
-                ec.presence_of_element_located((By.CSS_SELECTOR, "video.video-stream"))
-            )
-            player = self.browser.find_element(By.CSS_SELECTOR, "video.video-stream")
-            return not self.browser.execute_script("return arguments[0].paused", player)
-        except Exception as e:
-            logger.debug(f"Failed to check page status\nWindow: {self.current_url}")
-            return False
-
-
 def create_driver(driver_name):
+    logger.debug_general(f"Creating driver for: {driver_name}")
     if driver_name == 'chrome':  # doesnt work
         options = ChromeOptions()
         options.add_argument('--kiosk')
@@ -180,8 +117,53 @@ def create_driver(driver_name):
         browser = webdriver.Edge(options=options)
 
     else:
+        logger.error(f"Unsupported driver: {driver_name}")
         raise ValueError(f"Unsupported driver: {driver_name}")
+
+    logger.debug_general(f"Driver for {driver_name} created.")
     return browser
+
+
+class Monitor:
+    def __init__(self, browser, url=None):
+        self.browser = browser
+        self.current_url = url
+
+    def play_video(self, url):
+        if self.current_url == url:  # Prevent replaying the same video
+            return
+        try:
+            logger.debug_general(f'Video Started: {url}')
+            self.browser.get(url)
+            self.current_url = url
+            player = WebDriverWait(self.browser, 4).until(  # can probably be lower
+                ec.element_to_be_clickable((By.CSS_SELECTOR, "video.video-stream"))
+            )
+            player.send_keys('f', Keys.SPACE)
+        except Exception as e:
+            logger.error(f"Error playing video: {e}")
+
+    def close(self):
+        self.browser.quit()
+
+    def is_page_loaded(self):
+        try:
+            return self.browser.execute_script("return document.readyState") == "complete"
+        except Exception as e:
+            logger.error(f"Error checking page load status: {e}")
+            return False
+
+    def is_playing_video(self):
+        try:
+            # Wait for the video player element to be present on the page
+            WebDriverWait(self.browser, 4).until(  # can probably be lower
+                ec.presence_of_element_located((By.CSS_SELECTOR, "video.video-stream"))
+            )
+            player = self.browser.find_element(By.CSS_SELECTOR, "video.video-stream")
+            return not self.browser.execute_script("return arguments[0].paused", player)
+        except Exception as e:
+            logger.debug(f"Failed to check page status\nWindow: {self.current_url}")
+            return False
 
 
 class MonitorManager:
@@ -207,14 +189,16 @@ class MonitorManager:
 
     def stop(self):
         self.STOP_THREADS = True
-        time.sleep(2)  # Pause for a brief moment to allow any ongoing operations to complete
         self.cleanup_browsers()
-        self.active_videos.clear()  # Clear active_videos
+        self.active_videos.clear()
 
     def cleanup_browsers(self):
         """Close all browser windows."""
         for monitor in self.monitors:
-            monitor.close()
+            try:
+                monitor.close()
+            except Exception as e:
+                logger.error(f"Failed to close browser: {e}")
 
     def play_video_in_monitor(self, monitor):
         while not self.is_stopped():
@@ -228,23 +212,26 @@ class MonitorManager:
                             self.active_videos.discard(monitor.current_url)
             time.sleep(5)
 
-    def enqueue_videos(self, live_videos, top_videos):  # Might be broken now
+    def enqueue_videos(self, live_videos, top_videos):
+        logger.debug_general(f"Enqueuing {len(live_videos)} live videos and {len(top_videos)} top videos.")
         for video, priority in zip(live_videos + top_videos, [0] * len(live_videos) + [1] * len(top_videos)):
             if video not in self.active_videos:
                 self.video_queue.put((priority, video))
+        logger.debug_general(f"Videos enqueued. Total active videos: {len(self.active_videos)}")
 
     def init_browser_and_return_monitor(self, monitor):
         """This method initializes the browser and returns the monitor instance."""
         browser = create_driver(self.driver_name)
 
         logger.debug_selenium('Browser window opened')
-        screen_width, screen_height = monitor.width, monitor.height
         window_width, window_height = BROWSER_WINDOW_SIZE
-        position_x = (screen_width - window_width) // 2 + monitor.x
-        position_y = (screen_height - window_height) // 2 + monitor.y
+        position_x = (monitor.width - window_width) // 2 + monitor.x
+        position_y = (monitor.height - window_height) // 2 + monitor.y
 
-        browser.set_window_size(*BROWSER_WINDOW_SIZE)
+        # Use the monitor's width and height for the browser window size
+        browser.set_window_size(monitor.width, monitor.height)
         browser.set_window_position(position_x, position_y)
+        browser.maximize_window()
 
         monitor_instance = Monitor(browser)
         self.monitors.append(monitor_instance)
@@ -260,11 +247,13 @@ class MonitorManager:
         for next_live_video in next_live_videos:
             self.video_queue.put((0, next_live_video))
         self.live_videos = next_live_videos
-        self.prev_count = len(next_live_videos)
+        # Update this line to get the 'live' video count from the cache
+        self.prev_count = len(cache_manager.load_from_cache('live').get('urls', []))
 
     async def fetch_and_enqueue_next_videos(self):
         while not self.is_stopped():
-            result = await asyncio.get_running_loop().run_in_executor(None, monitor_youtube_streams, CHANNEL, self.driver_name)
+            result = await asyncio.get_running_loop().run_in_executor(None, monitor_youtube_streams, CHANNEL,
+                                                                      self.driver_name)
             if result > self.prev_count:
                 await self.fetch_next_live_videos()  # Fetch next set of live videos
                 self.enqueue_videos(self.live_videos, self.top_videos)  # Re-enqueue videos with updated live streams
@@ -299,6 +288,7 @@ class MonitorManager:
 
     def key_listener(self):
         keyboard.wait('esc')
+        logger.debug_general('Escape key hit')
         self.stop()
 
 
@@ -307,6 +297,7 @@ cache_manager = CacheManager(load_from_json())
 
 
 async def fetch_videos(event_type=None, order=None, max_results=None, page_token=None, bypass_cache=False):
+    logger.debug_general(f"Fetching videos with event_type: {event_type}, order: {order}")
     cache_key = event_type or order
 
     # Check cache
--- cacher.py
+++ cacher.py
@@ -8,49 +8,40 @@ from utility_helpers import save_to_json
 class CacheManager:
 
     def __init__(self, initial_cache=None):
-
         self.VIDEO_CACHE = initial_cache or {}
-
         self.logger = logging.getLogger(__name__)
+        self.logger.debug_general("CacheManager initialized.")
 
     def load_from_cache(self, key):
-
+        self.logger.debug_general(f"Loading data from cache for key: {key}")
         return self.VIDEO_CACHE.get(key)
 
     def save_to_cache(self, data, key):
-
         self.VIDEO_CACHE[key] = data
-
         save_to_json(data, key=key)
-
         self.logger.debug_general(f"Saved data to cache for key: {key}")
 
     def is_cache_valid(self, cache_key, caching_time=None):
+        self.logger.debug_general(f"Checking cache validity for key: {cache_key}")
 
         if cache_key in self.VIDEO_CACHE and 'last_updated' in self.VIDEO_CACHE[cache_key]:
-
             last_updated = datetime.datetime.strptime(self.VIDEO_CACHE[cache_key]['last_updated'], '%Y-%m-%d')
-
             caching_time = caching_time or (1 if cache_key == 'live' else 60)
-
             if (datetime.datetime.now() - last_updated).days < caching_time:
+                self.logger.debug_general(f"Cache for key: {cache_key} is valid.")
                 return True
 
         return False
 
     def update_and_save_cache(self, urls, page_token, cache_key):
-
+        self.logger.debug_general(f"Updating and saving cache for key: {cache_key}")
         if cache_key == "viewCount":
             existing_urls = self.load_from_cache('viewCount').get('urls', [])
-
             urls = list(set(existing_urls + urls))
 
         data_to_save = {
-
             'last_updated': datetime.datetime.now().strftime('%Y-%m-%d'),
-
             'urls': urls,
-
             'last_page_token': page_token  # Save the last page token
 
         }
